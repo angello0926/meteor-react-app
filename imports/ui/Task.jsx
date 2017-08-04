2ed702f9c5cd94d91acd4544c815c0d4b8d5629f
angello0926 <angel.lo@flocapital.com>
2017-08-04 18:01:50 +0800

    init

---
+++ imports/ui/Task.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from  'react';
+import { Tasks } from '../api/tasks.js';
+import PropTypes from 'prop-types';
+export default class Task extends React.Component {
+    toggleChecked(){
+        Tasks.update(this.props.task._id, {
+            $set: {
+                checked: !this.props.task.checked 
+            }
+        });
+    }
+
+    deleteTask(){
+        Tasks.remove(this.props.task._id);
+    }
+    render(){
+        let taskClassName = this.props.task.checked? 'checked': '';
+        return (
+            <li className={taskClassName}> 
+                <button className="delete" onClick={this.deleteTask.bind(this)}>
+                    &times;
+                </button>
+                <input type="checkbox" readOnly checked={this.props.task.checked } onClick={this.toggleChecked.bind(this)}/>
+                <span className="text">{ this.props.task.text}  </span>
+
+            </li>
+        );
+    }
+
+}
